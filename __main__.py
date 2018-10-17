8ae780b1936aca654524bc27ed96a6e0e619ccca
Matej Vitek <matej.vitek@hotmail.com>
2018-10-17 12:49:47 +0200

    Transfer to dingo

--- __main__.py
+++ __main__.py
@@ -3,28 +3,23 @@ import os
 from keras.applications import ResNet50
 from keras.preprocessing.image import ImageDataGenerator
 
+from validate import cross_validate
 
-DATA_DIR = r'.\Lozej\Datasets\Recognition'
-DATASET = 'Casia-Iris-Thousand'
-DATA = ('train', 'val', 'test')
+DATA_DIR = os.path.join('.', 'Lozej', 'Datasets', 'Recognition')
+DATASET = 'SBVPI'
+DATA = ('train', 'final_validation')
 
 
 def main():
 	dir = os.path.join(DATA_DIR, DATASET)
 	n_classes = sum(1 for c in os.listdir(os.path.join(dir, DATA[0])) if os.path.isdir(os.path.join(dir, DATA[0], c)))
-	model = ResNet50(weights=None, classes=n_classes)
-	model.compile(optimizer='sgd', loss='categorical_crossentropy')
+	
+	model = ResNet50(weights='imagenet', include_top=False)
+	train, test = generators_from_data(dir, size=model.input_shape[1:3])
+	for i, layer in enumerate(model.layers):
+		print(i, layer.name)
 
-	train, val, test = generators_from_data(dir, size=model.input_shape[1:3])
-
-	model.fit_generator(
-		train,
-		steps_per_epoch=5,#2000,
-		epochs=3,#50,
-		validation_data=val,
-		validation_steps=5#800
-	)
-	model.evaluate_generator(test)
+	#cross_validate(train, test, n_classes, model=model)
 
 
 def generators_from_data(dir, size=None):
