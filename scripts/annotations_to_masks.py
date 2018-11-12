b44d4f1e7c91635bd71358c19f474a7ad1899589
Matej Vitek <matej.vitek@hotmail.com>
2018-11-12 18:04:55 +0100

    Parallelism.

--- scripts/annotations_to_masks.py
+++ scripts/annotations_to_masks.py
@@ -1,3 +1,4 @@
+from joblib import Parallel, delayed
 import logging
 import matplotlib.image as img
 import numpy as np
@@ -33,75 +34,80 @@ def annotations_to_masks(source_dir, target_dir, save_type='json', **kwargs):
 		save_type = (save_type,)
 	save_type = list(map(str.lower, save_type))
 	all_types = any(t == 'all' for t in save_type)
-	
-	for i in os.listdir(source_dir):
-		source = os.path.join(source_dir, i)
-		if not os.path.isdir(source):
+
+	Parallel(n_jobs=-1, backend='multiprocessing')(
+		delayed(_process_images)(i, source_dir, target_dir, save_type, all_types)
+		for i in os.listdir(source_dir)
+	)
+
+
+def _process_images(i, source_dir, target_dir, save_type, all_types):
+	source = os.path.join(source_dir, i)
+	if not os.path.isdir(source):
+		return
+	target = os.path.join(target_dir, i)
+	os.makedirs(target, exist_ok=True)
+
+	for fname in os.listdir(source):
+		f = os.path.join(source, fname)
+		basename, ext = os.path.splitext(fname)
+		if not os.path.isfile(f) or ext.lower() not in IMG_EXTS or not re.fullmatch(r'\d+[LR]_[lrsu]_\d+', basename):
 			continue
-		target = os.path.join(target_dir, i)
-		os.makedirs(target, exist_ok=True)
-		
-		for fname in os.listdir(source):
-			f = os.path.join(source, fname)
-			basename, ext = os.path.splitext(fname)
-			if not os.path.isfile(f) or ext.lower() not in IMG_EXTS or not re.fullmatch(r'\d+[LR]_[lrsu]_\d', basename):
-				continue
-			print(fname)
-			img_size = img.imread(f).shape[:2]
-			shutil.copyfile(f, os.path.join(target, fname))
-			
-			mask = {}
-			for channel in PRIMARY_CHANNELS + SECONDARY_CHANNELS:
-				for img_ext in IMG_EXTS:
-					try:
-						annotation = img.imread(os.path.join(source, f'{basename}_{channel}{img_ext}'))[..., :3]
-					except FileNotFoundError:
-						continue
-					except OSError:
-						logging.error(f"Unexpected error while trying to read file {basename}{img_ext}. File may be corrupt.")
-						continue
-					mask[channel] = (annotation == (0, 1, 0)).all(axis=2)
-					if channel == 'periocular':
-						mask[channel] = ~mask[channel]
-					break
+		print(fname)
+		shutil.copyfile(f, os.path.join(target, fname))
+
+		mask = {}
+		for channel in PRIMARY_CHANNELS + SECONDARY_CHANNELS:
+			for img_ext in IMG_EXTS:
+				try:
+					annotation = img.imread(os.path.join(source, f'{basename}_{channel}{img_ext}'))[..., :3]
+				except FileNotFoundError:
+					continue
+				except OSError:
+					logging.error(f"Unexpected error while trying to read file {basename}{img_ext}. File may be corrupt.")
+					continue
+				mask[channel] = (annotation == (0, 1, 0)).all(axis=2)
+				if channel == 'periocular':
+					mask[channel] = ~mask[channel]
+				break
+			else:
+				if channel in PRIMARY_CHANNELS:
+					logging.warning(f"Missing primary channel {basename}_{channel}.")
 				else:
-					if channel in PRIMARY_CHANNELS:
-						logging.warning(f"Missing primary channel {basename}_{channel}.")
-					else:
-						logging.info(f"Missing secondary channel {basename}_{channel}.")
-			if not mask:
-				continue
+					logging.info(f"Missing secondary channel {basename}_{channel}.")
+		if not mask:
+			continue
 
-			save_name = os.path.join(target, '{}_{}{}')
-			
-			# Full image save
-			if all_types or any(t in save_type for t in ('img', 'image', 'mask', 'channels')):
-				for channel in mask:
-					img.imsave(save_name.format(basename, channel, '.png'), mask[channel], cmap='gray')
-			if any(f'{t}_full' in save_type for t in ('numpy', 'np', 'npy')):
-				for channel in mask:
-					np.save(save_name.format(basename, channel, '.npy'), mask[channel])
-			
-			# Sparse save
-			for channel in mask.keys():
-				mask[channel] = list(zip(*np.where(mask[channel])))
-			if all_types or any(t in save_type or f'{t}_sparse' in save_type for t in ('numpy', 'np', 'npy')):
-				for channel in mask:
-					np.save(save_name.format(basename, channel, '.npy'), np.array(mask[channel]))
+		save_name = os.path.join(target, '{}_{}{}')
 
-			# Sparse save as collections
-			for channel in mask.keys():
-				mask[channel] = [(int(x), int(y)) for (x, y) in mask[channel]]
-			if all_types or 'json' in save_type:
-				import json
-				with open(save_name.format(basename, 'channels', '.json'), 'w') as save_file:
-					json.dump(mask, save_file, indent=4, sort_keys=True)
-			if all_types or 'csv' in save_type:
-				import csv
-				with open(save_name.format(basename, 'channels', '.csv'), 'w') as save_file:
-					w = csv.DictWriter(save_file, mask.keys(), delimiter=';')
-					w.writeheader()
-					w.writerow(mask)
+		# Full image save
+		if all_types or any(t in save_type for t in ('img', 'image', 'mask', 'channels')):
+			for channel in mask:
+				img.imsave(save_name.format(basename, channel, '.png'), mask[channel], cmap='gray')
+		if any(f'{t}_full' in save_type for t in ('numpy', 'np', 'npy')):
+			for channel in mask:
+				np.save(save_name.format(basename, channel, '.npy'), mask[channel])
+
+		# Sparse save
+		for channel in mask.keys():
+			mask[channel] = list(zip(*np.where(mask[channel])))
+		if all_types or any(t in save_type or f'{t}_sparse' in save_type for t in ('numpy', 'np', 'npy')):
+			for channel in mask:
+				np.save(save_name.format(basename, channel, '.npy'), np.array(mask[channel]))
+
+		# Sparse save as collection
+		for channel in mask.keys():
+			mask[channel] = [(int(x), int(y)) for (x, y) in mask[channel]]
+		if all_types or 'json' in save_type:
+			import json
+			with open(save_name.format(basename, 'channels', '.json'), 'w') as save_file:
+				json.dump(mask, save_file, indent=4, sort_keys=True)
+		if all_types or 'csv' in save_type:
+			import csv
+			with open(save_name.format(basename, 'channels', '.csv'), 'w') as save_file:
+				w = csv.DictWriter(save_file, mask.keys(), delimiter=';')
+				w.writeheader()
+				w.writerow(mask)
 					
 						
 if __name__ == '__main__':
