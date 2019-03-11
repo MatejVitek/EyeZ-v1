87333c2ac76d2bb94c3e13d44fc85e111c58411c
Matej Vitek <matej.vitek.business@gmail.com>
2019-03-11 15:24:11 +0100

    Cleanup

--- scripts/segmentation_roc.py
+++ scripts/segmentation_roc.py
@@ -115,205 +115,203 @@ def rgb2gray(rgba):
 	return np.dot(rgba[...,:3], [0.2989, 0.587, 0.114])
 
 
-if __name__ == '__main__':
-	info = {model: [] for model in models}
+info = {model: [] for model in models}
 
-	if load_pr:
-		print("Loading existing precision/recall info")
-		existing = {model for model in models if os.path.isfile(pr_file.format(model))}
-		for model in existing:
-			with open(pr_file.format(model), 'rb') as f:
-				info[model] = pickle.load(f)
-		models = list(set(models) - existing)
+if load_pr:
+	print("Loading existing precision/recall info")
+	existing = {model for model in models if os.path.isfile(pr_file.format(model))}
+	for model in existing:
+		with open(pr_file.format(model), 'rb') as f:
+			info[model] = pickle.load(f)
+	models = list(set(models) - existing)
 
-	print("Computing precision/recall")
-	output = [x for x in Parallel(n_jobs=-1, backend='multiprocessing')(
-		delayed(_process_file)(pred_dir, model, file)
-		for model, file in itertools.chain.from_iterable(itertools.product([model], os.listdir(os.path.join(pred_dir, model))) for model in models)
-	) if x is not None]
-	for file in output:
-		info[file.model].append(file)
+print("Computing precision/recall")
+output = [x for x in Parallel(n_jobs=-1)(
+	delayed(_process_file)(pred_dir, model, file)
+	for model, file in itertools.chain.from_iterable(itertools.product([model], os.listdir(os.path.join(pred_dir, model))) for model in models)
+) if x is not None]
+for file in output:
+	info[file.model].append(file)
 
-	print(info.keys())
+print(info.keys())
 
-	# Save precision and recall
-	for model in models:
-		with open(pr_file.format(model), 'wb') as f:
-			pickle.dump(info[model], f)
+# Save precision and recall
+for model in models:
+	with open(pr_file.format(model), 'wb') as f:
+		pickle.dump(info[model], f)
 
-	print("Computing means and stds")
-	with open(f1_file, 'w') as score:
-		for model in tqdm(info):
-			pop_size = round(ratio * len(info[model]))
-			interp_points = 1000
-			max_f1 = np.empty((K, pop_size, 3))
-			auc_ = np.empty((K, pop_size))
-			precision = np.empty((K, pop_size, interp_points))
-			recall = np.empty((K, pop_size, interp_points))
+print("Computing means and stds")
+with open(f1_file, 'w') as score:
+	for model in tqdm(info):
+		pop_size = round(ratio * len(info[model]))
+		interp_points = 1000
+		max_f1 = np.empty((K, pop_size, 3))
+		auc_ = np.empty((K, pop_size))
+		precision = np.empty((K, pop_size, interp_points))
+		recall = np.empty((K, pop_size, interp_points))
 
-			for k in trange(K):
-				# Get a random sample population of size determined by ratio
-				shuffle(info[model])
-				files = info[model][:round(ratio * len(info[model]))]
+		for k in trange(K):
+			# Get a random sample population of size determined by ratio
+			shuffle(info[model])
+			files = info[model][:round(ratio * len(info[model]))]
 
-				for i, file in tqdm(enumerate(files)):
-					# Max F1 score and its corresponding precision/recall values
-					max_f1[k, i, :] = find_max_f1(file.precision, file.recall)
+			for i, file in tqdm(enumerate(files)):
+				# Max F1 score and its corresponding precision/recall values
+				max_f1[k, i, :] = find_max_f1(file.precision, file.recall)
 
-					# AUC
-					auc_[k, i] = auc(file.recall, file.precision)
+				# AUC
+				auc_[k, i] = auc(file.recall, file.precision)
 
-					# Precision/recall interpolation
-					precision[k, i, :] = interp1d(file.threshold, file.precision, fill_value='extrapolate')(np.linspace(0, 1, interp_points))
-					recall[k, i, :] = interp1d(file.threshold, file.recall, fill_value='extrapolate')(np.linspace(0, 1, interp_points))
+				# Precision/recall interpolation
+				precision[k, i, :] = interp1d(file.threshold, file.precision, fill_value='extrapolate')(np.linspace(0, 1, interp_points))
+				recall[k, i, :] = interp1d(file.threshold, file.recall, fill_value='extrapolate')(np.linspace(0, 1, interp_points))
 
-			# Compute mean and std of scores
-			score.write(f"{model}: {max_f1.mean((0, 1))} \u00B1 {max_f1.std((0, 1))}, AUC {auc_.mean()} \u00B1 {auc_.std()}\n")
+		# Compute mean and std of scores
+		score.write(f"{model}: {max_f1.mean((0, 1))} \u00B1 {max_f1.std((0, 1))}, AUC {auc_.mean()} \u00B1 {auc_.std()}\n")
 
-			# Plot mean and upper/lower std ROCs
-			pr = {}
-			for name, y in (('precision', precision), ('recall', recall)):
-				mean, std = y.mean((0, 1)), y.std((0, 1))
-				pr[name, 'mean'] = mean
-				pr[name, 'std1'] = mean + std
-				pr[name, 'std2'] = mean - std
+		# Plot mean and upper/lower std ROCs
+		pr = {}
+		for name, y in (('precision', precision), ('recall', recall)):
+			mean, std = y.mean((0, 1)), y.std((0, 1))
+			pr[name, 'mean'] = mean
+			pr[name, 'std1'] = mean + std
+			pr[name, 'std2'] = mean - std
 
-			c = next(colors)
-			plt.figure('ROC')
-			plt.plot(pr['recall', 'mean'], pr['precision', 'mean'], label=model, linewidth=2, color=c)
-			for std in ('std1', 'std2'):
-				plt.plot(pr['recall', std], pr['precision', std], ':', linewidth=1, color=c)
-			plt.figure('Zoomed ROC')
-			plt.plot(pr['recall', 'mean'], pr['precision', 'mean'], label=model, linewidth=2, color=c)
-			for std in ('std1', 'std2'):
-				plt.plot(pr['recall', std], pr['precision', std], ':', linewidth=1, color=c)
-			#plt.plot(max_f1.mean((0, 1))[1], max_f1.mean((0, 1))[0], 'o', markersize=15, color=c)	
+		c = next(colors)
+		plt.figure('ROC')
+		plt.plot(pr['recall', 'mean'], pr['precision', 'mean'], label=model, linewidth=2, color=c)
+		for std in ('std1', 'std2'):
+			plt.plot(pr['recall', std], pr['precision', std], ':', linewidth=1, color=c)
+		plt.figure('Zoomed ROC')
+		plt.plot(pr['recall', 'mean'], pr['precision', 'mean'], label=model, linewidth=2, color=c)
+		for std in ('std1', 'std2'):
+			plt.plot(pr['recall', std], pr['precision', std], ':', linewidth=1, color=c)
+		#plt.plot(max_f1.mean((0, 1))[1], max_f1.mean((0, 1))[0], 'o', markersize=15, color=c)
 
-			# Max f1 on mean plot
-			max_f1_mean = find_max_f1(pr['precision', 'mean'], pr['recall', 'mean'])
-			plt.plot(max_f1_mean[1], max_f1_mean[0], 'o', markersize=15, color=c)
+		# Max f1 on mean plot
+		max_f1_mean = find_max_f1(pr['precision', 'mean'], pr['recall', 'mean'])
+		plt.plot(max_f1_mean[1], max_f1_mean[0], 'o', markersize=15, color=c)
 
 
-	plt.figure('ROC')
-	plt.grid()
-	plt.gca().xaxis.set_major_formatter(FuncFormatter(format_bin_label))
-	plt.gca().yaxis.set_major_formatter(FuncFormatter(format_bin_label))
-	plt.margins(0)
-	plt.xlabel('Recall')
-	plt.ylabel('Precision')
-	plt.xlim(0, 1.01)
-	plt.ylim(0, 1.01)
-	plt.xticks(np.linspace(0.2, 1, 5))
-	plt.yticks(np.linspace(0, 1, 6))
-	plt.legend(loc=legend_loc, fontsize=legend_size)
-	plt.savefig(fig_file, bbox_inches='tight')
-	plt.tight_layout(pad=0)
+plt.figure('ROC')
+plt.grid()
+plt.gca().xaxis.set_major_formatter(FuncFormatter(format_bin_label))
+plt.gca().yaxis.set_major_formatter(FuncFormatter(format_bin_label))
+plt.margins(0)
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.xlim(0, 1.01)
+plt.ylim(0, 1.01)
+plt.xticks(np.linspace(0.2, 1, 5))
+plt.yticks(np.linspace(0, 1, 6))
+plt.legend(loc=legend_loc, fontsize=legend_size)
+plt.savefig(fig_file, bbox_inches='tight')
+plt.tight_layout(pad=0)
 
-	plt.figure('Zoomed ROC')
-	plt.grid()
-	plt.gca().xaxis.set_major_formatter(FuncFormatter(format_bin_label))
-	plt.gca().yaxis.set_major_formatter(FuncFormatter(format_bin_label))
-	plt.margins(0)
-	plt.xlabel('Recall')
-	plt.ylabel('Precision')
-	plt.xlim(0.9, 1.001)
-	plt.ylim(0.9, 1.001)
-	plt.xticks(np.linspace(0.92, 1, 5))
-	plt.yticks(np.linspace(0.9, 1, 6))
-	plt.legend(loc='upper right', fontsize=legend_size, borderpad=0.2, borderaxespad=0.2, labelspacing=0.2)
-	plt.savefig(zoom_file, bbox_inches='tight')
-	plt.tight_layout(pad=0)
+plt.figure('Zoomed ROC')
+plt.grid()
+plt.gca().xaxis.set_major_formatter(FuncFormatter(format_bin_label))
+plt.gca().yaxis.set_major_formatter(FuncFormatter(format_bin_label))
+plt.margins(0)
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.xlim(0.9, 1.001)
+plt.ylim(0.9, 1.001)
+plt.xticks(np.linspace(0.92, 1, 5))
+plt.yticks(np.linspace(0.9, 1, 6))
+plt.legend(loc='upper right', fontsize=legend_size, borderpad=0.2, borderaxespad=0.2, labelspacing=0.2)
+plt.savefig(zoom_file, bbox_inches='tight')
+plt.tight_layout(pad=0)
 
-	plt.show()
+plt.show()
 
 
 '''
 The above bootstrapping computes mean and std over all pictures and all folds
 The below bootstrapping only computes mean and std over folds, first computing the mean precision/recall of all pictures in population for every fold
 
-if __name__ == '__main__':
-	info = {model: [] for model in models}
-	
-	if load_pr:
-		print("Loading existing precision/recall info")
-		existing = {model for model in models if os.path.isfile(pr_file.format(model))}
-		for model in existing:
-			with open(pr_file.format(model), 'rb') as f:
-				info[model] = pickle.load(f)
-		models = list(set(models) - existing)
-	
-	print("Computing precision/recall")
-	output = [x for x in Parallel(n_jobs=-1, backend='multiprocessing')(
-		delayed(_process_file)(pred_dir, model, file)
-		for model, file in itertools.chain.from_iterable(itertools.product([model], os.listdir(os.path.join(pred_dir, model))) for model in models)
-	) if x is not None]
-	for file in output:
-		info[file.model].append(file)
-	
-	print(info.keys())
-	
-	# Save precision and recall
-	for model in models:
-		with open(pr_file.format(model), 'wb') as f:
-			pickle.dump(info[model], f)
+info = {model: [] for model in models}
 
-	print("Computing means and stds")
-	with open(f1_file, 'w') as score:
-		for model in tqdm(info):
-			pop_size = round(ratio * len(info[model]))
-			interp_points = 1000
-			max_f1 = np.empty((K, 3))
-			auc_ = np.empty(K)
-			precision = np.empty((K, interp_points + 1))
-			recall = np.empty((K, interp_points + 1))
+if load_pr:
+	print("Loading existing precision/recall info")
+	existing = {model for model in models if os.path.isfile(pr_file.format(model))}
+	for model in existing:
+		with open(pr_file.format(model), 'rb') as f:
+			info[model] = pickle.load(f)
+	models = list(set(models) - existing)
+
+print("Computing precision/recall")
+output = [x for x in Parallel(n_jobs=-1, backend='multiprocessing')(
+	delayed(_process_file)(pred_dir, model, file)
+	for model, file in itertools.chain.from_iterable(itertools.product([model], os.listdir(os.path.join(pred_dir, model))) for model in models)
+) if x is not None]
+for file in output:
+	info[file.model].append(file)
+
+print(info.keys())
+
+# Save precision and recall
+for model in models:
+	with open(pr_file.format(model), 'wb') as f:
+		pickle.dump(info[model], f)
+
+print("Computing means and stds")
+with open(f1_file, 'w') as score:
+	for model in tqdm(info):
+		pop_size = round(ratio * len(info[model]))
+		interp_points = 1000
+		max_f1 = np.empty((K, 3))
+		auc_ = np.empty(K)
+		precision = np.empty((K, interp_points + 1))
+		recall = np.empty((K, interp_points + 1))
+		
+		for k in trange(K):
+			# Get a random sample population of size determined by ratio
+			shuffle(info[model])
+			files = info[model][:round(ratio * len(info[model]))]
 			
-			for k in trange(K):
-				# Get a random sample population of size determined by ratio
-				shuffle(info[model])
-				files = info[model][:round(ratio * len(info[model]))]
-				
-				# Get mean precision/recall of population
-				precision[k, :-1] = np.array([
-					interp1d(file.threshold, file.precision, fill_value='extrapolate')(np.linspace(0, 1, interp_points))
-					for file in files
-				]).mean(0).clip(0., 1.)
-				recall[k, :-1] = np.array([
-					interp1d(file.threshold, file.recall, fill_value='extrapolate')(np.linspace(0, 1, interp_points))
-					for file in files
-				]).mean(0).clip(0., 1.)
-				
-				# Fix for bad recall extrapolation
-				precision[k, -1] = precision[k, -2]
-				recall[k, -1] = 0.
-				
-				# Max F1 score and its corresponding precision/recall values
-				max_f1[k, :] = find_max_f1(precision[k, :], recall[k, :])
-				# AUC
-				auc_[k] = auc(recall[k, :], precision[k, :])
+			# Get mean precision/recall of population
+			precision[k, :-1] = np.array([
+				interp1d(file.threshold, file.precision, fill_value='extrapolate')(np.linspace(0, 1, interp_points))
+				for file in files
+			]).mean(0).clip(0., 1.)
+			recall[k, :-1] = np.array([
+				interp1d(file.threshold, file.recall, fill_value='extrapolate')(np.linspace(0, 1, interp_points))
+				for file in files
+			]).mean(0).clip(0., 1.)
 			
-			# Compute mean and std of scores
-			score.write(f"{model}: {max_f1.mean(0)} \u00B1 {max_f1.std(0)}, AUC {auc_.mean()} \u00B1 {auc_.std()}\n")
+			# Fix for bad recall extrapolation
+			precision[k, -1] = precision[k, -2]
+			recall[k, -1] = 0.
 			
-			# Plot mean and upper/lower std ROCs
-			pr = {}
-			for name, y in (('precision', precision), ('recall', recall)):
-				mean, std = y.mean(0), y.std(0)
-				pr[name, 'mean'] = mean
-				pr[name, 'std1'] = mean + std
-				pr[name, 'std2'] = mean - std
-			
-			c = next(colors)
-			plt.figure('ROC')
-			plt.plot(pr['recall', 'mean'], pr['precision', 'mean'], label=model, linewidth=2, color=c)
-			for std in ('std1', 'std2'):
-				plt.plot(pr['recall', std], pr['precision', std], ':', linewidth=1, color=c)
-			plt.figure('Zoomed ROC')
-			plt.plot(pr['recall', 'mean'], pr['precision', 'mean'], label=model, linewidth=2, color=c)
-			for std in ('std1', 'std2'):
-				plt.plot(pr['recall', std], pr['precision', std], ':', linewidth=1, color=c)
-			#plt.plot(max_f1.mean(0)[1], max_f1.mean(0)[0], 'o', markersize=15, color=c)
-			
-			# Max f1 on mean plot
-			max_f1_mean = find_max_f1(pr['precision', 'mean'], pr['recall', 'mean'])
-			plt.plot(max_f1_mean[1], max_f1_mean[0], 'o', markersize=15, color=c)
+			# Max F1 score and its corresponding precision/recall values
+			max_f1[k, :] = find_max_f1(precision[k, :], recall[k, :])
+			# AUC
+			auc_[k] = auc(recall[k, :], precision[k, :])
+		
+		# Compute mean and std of scores
+		score.write(f"{model}: {max_f1.mean(0)} \u00B1 {max_f1.std(0)}, AUC {auc_.mean()} \u00B1 {auc_.std()}\n")
+		
+		# Plot mean and upper/lower std ROCs
+		pr = {}
+		for name, y in (('precision', precision), ('recall', recall)):
+			mean, std = y.mean(0), y.std(0)
+			pr[name, 'mean'] = mean
+			pr[name, 'std1'] = mean + std
+			pr[name, 'std2'] = mean - std
+		
+		c = next(colors)
+		plt.figure('ROC')
+		plt.plot(pr['recall', 'mean'], pr['precision', 'mean'], label=model, linewidth=2, color=c)
+		for std in ('std1', 'std2'):
+			plt.plot(pr['recall', std], pr['precision', std], ':', linewidth=1, color=c)
+		plt.figure('Zoomed ROC')
+		plt.plot(pr['recall', 'mean'], pr['precision', 'mean'], label=model, linewidth=2, color=c)
+		for std in ('std1', 'std2'):
+			plt.plot(pr['recall', std], pr['precision', std], ':', linewidth=1, color=c)
+		#plt.plot(max_f1.mean(0)[1], max_f1.mean(0)[0], 'o', markersize=15, color=c)
+		
+		# Max f1 on mean plot
+		max_f1_mean = find_max_f1(pr['precision', 'mean'], pr['recall', 'mean'])
+		plt.plot(max_f1_mean[1], max_f1_mean[0], 'o', markersize=15, color=c)
 '''
